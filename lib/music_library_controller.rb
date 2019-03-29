1ba01d56371fd7d83bc0a03337fda522acf603ea
Ryan Wolfe <ryan@studiohopfitness.com>
2019-03-29 18:58:28 +0000

    Automatically backed up by Learn

--- lib/music_library_controller.rb
+++ lib/music_library_controller.rb
@@ -56,7 +56,7 @@ class MusicLibraryController
     input = gets.strip
 
     if artist = Artist.find_by_name(input)
-      artist.songs.sort {|x, y| x.name <=> y.name}.each_with_index{ |song, index| puts "#{index += 1}. #{song.artist.name} - #{song.name} - #{song.genre.name}"}
+      artist.songs.sort {|x, y| x.name <=> y.name}.each_with_index{ |song, index| puts "#{index += 1}. #{song.name} - #{song.genre.name}"}
     end
 
   end
